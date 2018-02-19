880da2450a681e5c7952082a51eee87070803ccc
Konstantin Metto <konstantin.metto@codeswat.com>
2018-02-19 19:16:32 +0300

    added webpack config and components export

---
+++ src/components/index.js
@@ -0,0 +1,153 @@
+import Accordion from './Accordion/Accordion.vue';
+import ActivityTimeline from './ActivityTimeline/ActivityTimeline.vue';
+import Alert from './Alert/Alert.vue';
+import AppLauncher from './AppLauncher/AppLauncher.vue';
+import Avatar from './Avatar/Avatar.vue';
+import Badges from './Badges/Badges.vue';
+import BrandBand from './BrandBand/BrandBand.vue';
+import Breadcrumbs from './Breadcrumbs/Breadcrumbs.vue';
+import ButtonGroups from './ButtonGroups/ButtonGroups.vue';
+import ButtonIcons from './ButtonIcons/ButtonIcons.vue';
+import Buttons from './Buttons/Buttons.vue';
+import Cards from './Cards/Cards.vue';
+import Carousel from './Carousel/Carousel.vue';
+import Checkbox from './Checkbox/Checkbox.vue';
+import CheckboxButton from './CheckboxButton/CheckboxButton.vue';
+import ChecCheckboxButtonGroupkboxButton from './CheckboxButtonGroup/CheckboxButtonGroup.vue';
+import CheckboxToggle from './CheckboxToggle/CheckboxToggle.vue';
+import ColorPicker from './ColorPicker/ColorPicker.vue';
+import Combobox from './Combobox/Combobox.vue';
+import DataTables from './DataTables/DataTables.vue';
+import Datepickers from './Datepickers/Datepickers.vue';
+import DatetimePicker from './DatetimePicker/DatetimePicker.vue';
+import DockedComposer from './DockedComposer/DockedComposer.vue';
+import DockedFormFooter from './DockedFormFooter/DockedFormFooter.vue';
+import DockedUtilityBar from './DockedUtilityBar/DockedUtilityBar.vue';
+import DuelingPicklist from './DuelingPicklist/DuelingPicklist.vue';
+import DynamicIcons from './DynamicIcons/DynamicIcons.vue';
+import DynamicMenu from './DynamicMenu/DynamicMenu.vue';
+import ExpandableSection from './ExpandableSection/ExpandableSection.vue';
+import Feeds from './Feeds/Feeds.vue';
+import Files from './Files/Files.vue';
+import FileSelector from './FileSelector/FileSelector.vue';
+import FormElement from './FormElement/FormElement.vue';
+import FormLayout from './FormLayout/FormLayout.vue';
+import GlobalHeader from './GlobalHeader/GlobalHeader.vue';
+import GlobalValidation from './GlobalValidation/GlobalValidation.vue';
+import Icons from './Icons/Icons.vue';
+import Input from './Input/Input.vue';
+import ListBuilder from './ListBuilder/ListBuilder.vue';
+import Lookups from './Lookups/Lookups.vue';
+import Map from './Map/Map.vue';
+import Menus from './Menus/Menus.vue';
+import Modals from './Modals/Modals.vue';
+import Notifications from './Notifications/Notifications.vue';
+import PageHeaders from './PageHeaders/PageHeaders.vue';
+import Panels from './Panels/Panels.vue';
+import Path from './Path/Path.vue';
+import PickList from './PickList/PickList.vue';
+import Pills from './Pills/Pills.vue';
+import Popovers from './Popovers/Popovers.vue';
+import ProgressBar from './ProgressBar/ProgressBar.vue';
+import ProgressIndicator from './ProgressIndicator/ProgressIndicator.vue';
+import ProgressRing from './ProgressRing/ProgressRing.vue';
+import Prompt from './Prompt/Prompt.vue';
+import Publishers from './Publishers/Publishers.vue';
+import RadioButtonGroup from './RadioButtonGroup/RadioButtonGroup.vue';
+import RadioGroup from './RadioGroup/RadioGroup.vue';
+import RichTextEditor from './RichTextEditor/RichTextEditor.vue';
+import ScopedNotifications from './ScopedNotifications/ScopedNotifications.vue';
+import ScopedTabs from './ScopedTabs/ScopedTabs.vue';
+import Select from './Select/Select.vue';
+import Slider from './Slider/Slider.vue';
+import Spinners from './Spinners/Spinners.vue';
+import SplitView from './SplitView/SplitView.vue';
+import Tabs from './Tabs/Tabs.vue';
+import Textarea from './Textarea/Textarea.vue';
+import Tiles from './Tiles/Tiles.vue';
+import Timepicker from './Timepicker/Timepicker.vue';
+import Toast from './Toast/Toast.vue';
+import Tooltips from './Tooltips/Tooltips.vue';
+import Trees from './Trees/Trees.vue';
+import VerticalNavigation from './VerticalNavigation/VerticalNavigation.vue';
+import VerticalTabs from './VerticalTabs/VerticalTabs.vue';
+import VisualPicker from './VisualPicker/VisualPicker.vue';
+import Walkthrough from './Walkthrough/Walkthrough.vue';
+
+export {
+  Accordion,
+  ActivityTimeline,
+  Alert,
+  AppLauncher,
+  Avatar,
+  Badges,
+  BrandBand,
+  Breadcrumbs,
+  ButtonGroups,
+  ButtonIcons,
+  Buttons,
+  Cards,
+  Carousel,
+  Checkbox,
+  CheckboxButton,
+  ChecCheckboxButtonGroupkboxButton,
+  CheckboxToggle,
+  ColorPicker,
+  Combobox,
+  DataTables,
+  Datepickers,
+  DatetimePicker,
+  DockedComposer,
+  DockedFormFooter,
+  DockedUtilityBar,
+  DuelingPicklist,
+  DynamicIcons,
+  DynamicMenu,
+  ExpandableSection,
+  Feeds,
+  Files,
+  FileSelector,
+  FormElement,
+  FormLayout,
+  GlobalHeader,
+  GlobalValidation,
+  Icons,
+  Input,
+  ListBuilder,
+  Lookups,
+  Map,
+  Menus,
+  Modals,
+  Notifications,
+  PageHeaders,
+  Panels,
+  Path,
+  PickList,
+  Pills,
+  Popovers,
+  ProgressBar,
+  ProgressIndicator,
+  ProgressRing,
+  Prompt,
+  Publishers,
+  RadioButtonGroup,
+  RadioGroup,
+  RichTextEditor,
+  ScopedNotifications,
+  ScopedTabs,
+  Select,
+  Slider,
+  Spinners,
+  SplitView,
+  Tabs,
+  Textarea,
+  Tiles,
+  Timepicker,
+  Toast,
+  Tooltips,
+  Trees,
+  VerticalNavigation,
+  VerticalTabs,
+  VisualPicker,
+  Walkthrough,
+};
