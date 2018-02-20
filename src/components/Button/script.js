fcac4342a231d8e522c98350dad850dd8d8bb8d4
Konstantin Metto <konstantin.metto@codeswat.com>
2018-02-20 14:31:37 +0300

    added base functionality for Button component

---
+++ src/components/Button/script.js
@@ -0,0 +1,40 @@
+export default {
+  name: 'slds-button',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    neutral: {
+      type: Boolean,
+      default: false,
+    },
+    brand: {
+      type: Boolean,
+      default: false,
+    },
+    inverse: {
+      type: Boolean,
+      default: false,
+    },
+    destructive: {
+      type: Boolean,
+      default: false,
+    },
+    success: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      classes: {
+        'slds-button_neutral': this.neutral,
+        'slds-button_brand': this.brand,
+        'slds-button_inverse': this.inverse,
+        'slds-button_destructive': this.destructive,
+        'slds-button_success': this.success,
+      }
+    };
+  },
+};
