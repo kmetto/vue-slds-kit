4a618dba03d0050fd778496776d2d3bbbd0bc1bc
Konstantin Metto <konstantin.metto@codeswat.com>
2018-02-20 17:27:27 +0300

    prompt component

--- src/components/Prompt/script.js
+++ src/components/Prompt/script.js
@@ -1,3 +1,25 @@
+import SldsButton from '../Button/Button.vue';
+
 export default {
   name: 'slds-prompt',
+  components: [
+    SldsButton,
+  ],
+  data(){
+    return {
+      isVisible: false,
+    };
+  },
+  methods: {
+    show(){
+      this.isVisible = true;
+    },
+    hide(){
+      this.isVisible = false;
+    },
+    onClose() {
+      this.hide();
+      this.$emit('onclose');
+    },
+  },
 };
